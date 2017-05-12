36c0425879eead391cbcbbc877b2fe8f25723ea3
Luay Suarna <muhamad.luay@gmail.com>
2017-05-12 14:13:09 +0700

    First init

---
+++ FoodPin/Restaurant.swift
@@ -0,0 +1,29 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 20/8/15.
+//  Copyright © 2015 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var phoneNumber = ""
+    var image = ""
+    var isVisited = false
+    var rating = ""
+    
+    init(name:String, type:String, location:String, phoneNumber:String, image:String, isVisited:Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.phoneNumber = phoneNumber
+        self.image = image
+        self.isVisited = isVisited
+    }
+}
+
